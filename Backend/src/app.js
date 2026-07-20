793a808be31c6cd2badd5c05d4d17e2b7b8d8970
khushboo <rkhushboo5477@gmail.com>
2026-07-20 16:39:20 +0530

    Initial commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app =express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interview-prep-cylp.onrender.com",
     credentials:true
 }))
 //require all the routes here//
