d06c56134fd31fb9e64bba06920d82bc4b1d1739
khushboo <rkhushboo5477@gmail.com>
2026-07-24 10:30:39 +0530

    initial commit

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,66 +1,50 @@
-import { useContext, useEffect } from "react";
-import { AuthContext } from "../auth.context";
-import { login, register, logout, getMe } from "../services/auth.api";
+import React,{useState} from 'react'
+import { useNavigate, Link } from 'react-router'
+import "../auth.form.scss"
+import { useAuth } from '../hooks/useAuth'
 
+const Login = () => {
 
+    const { loading, handleLogin } = useAuth()
+    const navigate = useNavigate()
 
-export const useAuth = () => {
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
 
-    const context = useContext(AuthContext)
-    const { user, setUser, loading, setLoading } = context
-
-
-    const handleLogin = async ({ email, password }) => {
-        setLoading(true)
-        try {
-            const data = await login({ email, password })
-            setUser(data.user)
-        } catch (err) {
-
-        } finally {
-            setLoading(false)
-        }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await handleLogin({email,password})
+        navigate('/')
     }
 
-    const handleRegister = async ({ username, email, password }) => {
-        setLoading(true)
-        try {
-            const data = await register({ username, email, password })
-            setUser(data.user)
-        } catch (err) {
-
-        } finally {
-            setLoading(false)
-        }
+    if(loading){
+        return (<main><h1>Loading.......</h1></main>)
     }
 
-    const handleLogout = async () => {
-        setLoading(true)
-        try {
-            const data = await logout()
-            setUser(null)
-        } catch (err) {
 
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-
-        const getAndSetUser = async () => {
-            try {
-
-                const data = await getMe()
-                setUser(data.user)
-            } catch (err) { } finally {
-                setLoading(false)
-            }
-        }
-
-        getAndSetUser()
-
-    }, [])
-
-    return { user, loading, handleRegister, handleLogin, handleLogout }
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="email" id="email" name='email' placeholder='Enter email address' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="password" id="password" name='password' placeholder='Enter password' />
+                    </div>
+                    <button className='button primary-button' >Login</button>
+                </form>
+                <p>Don't have an account? <Link to={"/register"} >Register</Link> </p>
+            </div>
+        </main>
+    )
 }
+
+export default Login
